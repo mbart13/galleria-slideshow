a764bebae5c1139f479ca97a43338f0e9f6a9a0c
Michal Bartosik <msbartosik@gmail.com>
2021-08-11 10:23:48 +0200

    change delay time

--- src/components/organisms/Slide/index.tsx
+++ src/components/organisms/Slide/index.tsx
@@ -47,7 +47,7 @@ const Slide = (): JSX.Element => {
           <Figure
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            transition={{ duration: 2 }}
+            transition={{ duration: 1.5 }}
           >
             <picture>
               <source
